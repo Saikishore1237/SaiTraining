fdff2d65f960c0e6075c82f1949377ba2a5ff687
Saikishore <saikishoresri@gmail.com>
2025-03-10 18:21:18 +0000

    1D Arrays Concepts-2

--- src/Mar10/OneDArrays.java
+++ src/Mar10/OneDArrays.java
@@ -1,4 +1,28 @@
 package Mar10;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class OneDArrays {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        System.out.println(a);
+        System.out.println(a.length);
+        for (int i=0; i<a.length; i++)
+        {
+            a[i] = i*i;
+            System.out.print(a[i]);
+        }
+
+        System.out.println(Arrays.toString(a));
+        Arrays.fill(a, 40);
+        System.out.println(Arrays.toString(a));
+
+        int b[] = Arrays.copyOf(a,8);
+        System.out.println(Arrays.toString(b));
+
+        int c[]=Arrays.copyOfRange(a,4,8);
+        System.out.println(Arrays.toString(c));
+
+    }
 }
