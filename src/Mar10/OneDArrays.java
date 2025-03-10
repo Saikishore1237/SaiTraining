9fb8d4e8887012b9631d9a0f24256305e8890bdb
Saikishore <saikishoresri@gmail.com>
2025-03-10 19:08:14 +0000

    More string commands

--- src/Mar10/OneDArrays.java
+++ src/Mar10/OneDArrays.java
@@ -24,5 +24,19 @@ public class OneDArrays {
         int c[]=Arrays.copyOfRange(a,4,8);
         System.out.println(Arrays.toString(c));
 
+        String str[] = {"Hi", "Hello", "Adab", "Namasthe"};
+
+        System.out.println(str);
+        System.out.println(Arrays.toString(str));
+        System.out.println(str.length);
+
+        String str2[] = str;
+
+        System.out.println("str2 string is "+Arrays.toString(str2));
+        System.out.println(Arrays.mismatch(str, str2));
+
+        String str3[]=str.clone();
+        System.out.println(Arrays.toString(str3));
+
     }
 }
